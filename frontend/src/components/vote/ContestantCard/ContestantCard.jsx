7ecd7949831cde3add00176579a88977adccc3d8
C.Mario <40488893+iRonic94@users.noreply.github.com>
2026-09-07 11:06:24 +0300

    Update image path to use contestant name_id

--- frontend/src/components/vote/ContestantCard/ContestantCard.jsx
+++ frontend/src/components/vote/ContestantCard/ContestantCard.jsx
@@ -7,7 +7,7 @@ function ContestantCard({
     onSelect,
     disabled,
 }) {
-    const imagePath = `${concurent.gender}.webp`;
+    const imagePath = `${concurent.name_id}.webp`;
 
     const { data } = supabase.storage
         .from('cupluri')
